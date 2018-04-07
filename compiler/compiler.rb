8a1b93e58bd717959bfe5877e064e6ae1643d2af
Walerian <walerian.sobczak@getbase.com>
2018-04-08 00:15:38 +0200

    A Compiler From Scratch #2
    Parser.

--- compiler/compiler.rb
+++ compiler/compiler.rb
@@ -7,7 +7,8 @@ class Tokenizer
     [:identifier, /\b[a-zA-Z]+\b/],
     [:integer, /\b[0-9]+\b/],
     [:oparen, /\(/],
-    [:cparen, /\)/]
+    [:cparen, /\)/],
+    [:comma, /,/]
   ].freeze
 
   def initialize(code)
@@ -26,11 +27,11 @@ class Tokenizer
   def tokenize_one_token
     TOKEN_TYPES.each do |(type, re)|
       re = /\A(#{re})/
-      if @code =~ re
-        value = $1
-        @code = @code[value.length..-1]
-        return Token.new(type, value)
-      end
+      next unless @code =~ re
+
+      value = $1
+      @code = @code[value.length..-1]
+      return Token.new(type, value)
     end
 
     raise "Couldn't match token on #{@code.inspect}"
@@ -39,6 +40,101 @@ end
 
 Token = Struct.new(:type, :value)
 
+class Parser
+  def initialize(tokens)
+    @tokens = tokens
+  end
+
+  def parse
+    parse_def
+  end
+
+  def parse_def
+    consume(:def)
+    name = consume(:identifier).value
+    arg_names = parse_arg_names
+    body = parse_expr
+    consume(:end)
+    DefNode.new(name, arg_names, body)
+  end
+
+  def parse_arg_names
+    arg_names = []
+
+    consume(:oparen)
+    if peek(:identifier)
+      arg_names << consume(:identifier).value
+      while peek(:comma)
+        consume(:comma)
+        arg_names << consume(:identifier).value
+      end
+    end
+    consume(:cparen)
+
+    arg_names
+  end
+
+  def parse_expr
+    if peek(:integer)
+      parse_integer
+    elsif peek(:identifier) && peek(:oparen, 1)
+      parse_call
+    else
+      parse_var_ref
+    end
+  end
+
+  def parse_integer
+    IntegerNode.new(consume(:integer).value.to_i)
+  end
+
+  def parse_call
+    name = consume(:identifier)
+    arg_exprs = parse_arg_exprs
+    CallNode.new(name, arg_exprs)
+  end
+
+  def parse_arg_exprs
+    arg_exprs = []
+
+    consume(:oparen)
+    unless peek(:cparen)
+      arg_exprs << parse_expr
+      while peek(:comma)
+        consume(:comma)
+        arg_exprs << parse_expr
+      end
+    end
+    consume(:cparen)
+
+    arg_exprs
+  end
+
+  def parse_var_ref
+    VarRefNode.new(consume(:identifier).value)
+  end
+
+  def consume(expected_type)
+    token = @tokens.shift
+    return token if token.type == expected_type
+
+    raise "Expected token type #{expected_type.inspect} but got #{token.type.inspect}"
+  end
+
+  def peek(expected_type, offset = 0)
+    @tokens.fetch(offset).type == expected_type
+  end
+end
+
+DefNode = Struct.new(:name, :arg_names, :body)
+IntegerNode = Struct.new(:value)
+CallNode = Struct.new(:name, :arg_exprs)
+VarRefNode = Struct.new(:value)
+
 source_file = File.read('test.src')
+
 tokens = Tokenizer.new(source_file).tokenize
 p tokens.map(&:inspect)
+
+tree = Parser.new(tokens).parse
+p tree
